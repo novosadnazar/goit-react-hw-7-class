d33238cefabcec6aaceb1d69c463c97e5c7b5f5c
novosadnazar <163895331+novosadnazar@users.noreply.github.com>
2026-03-12 20:15:51 +0200

    fixed

--- src/App.js
+++ src/App.js
@@ -1,51 +1,12 @@
-import React from "react";
-import style from "./App.module.css";
+import "modern-normalize";
+import { TaskList } from "./components/TasksList/TaskList";
 
-export default class App extends React.Component {
-
-  static defaultProps = {
-    initialState: 0,
-  };
-
-
-  state = {
-    value: this.props.initialState,
-  };
-
-
-  handlePlus = () => {
-    this.setState((prevState) => {
-      return {
-        value: prevState.value + 1,
-      };
-    });
-  };
-
-  handleMinus = () => {
-    this.setState((prevState) => ({ value: prevState.value - 1 }));
-  };
-
-  render() {
-    return (
-      <div className={style.box}>
-        <span className={style.value}>{this.state.value}</span>
-        <div className={style.wrapper}>
-          <button
-            onClick={this.handlePlus} 
-            type="button"
-            className={style.btn}
-          >
-            Збільшити
-          </button>
-          <button
-            onClick={this.handleMinus}
-            type="button"
-            className={style.btn}
-          >
-            Зменшити
-          </button>
-        </div>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <>
+      <TaskList />
+    </>
+  );
 }
+
+export default App;
---
+++ src/components/TasksList/TaskList.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import style from "./TaskList.module.css";
+import { nanoid } from "nanoid";
+export class TaskList extends Component {
+  state = {
+    tasks: [
+      { id: 1, text: "вивчити react" },
+      { id: 2, text: "більше практикуватися" },
+      { id: 3, text: "не забувати повторювати JS" },
+    ],
+    newTaskText: "",
+  };
+
+  handleDelete = (taskId) => {
+    this.setState({
+      tasks: this.state.tasks.filter(({ id }) => {
+        return id !== taskId;
+      }),
+    });
+  };
+
+  handleInput = (evt) => {
+    this.setState({ newTaskText: evt.target.value });
+  };
+
+  handleAdd = () => {
+    const text = this.state.newTaskText;
+    if (text === "") {
+      return;
+    }
+    const newTask = { id: nanoid(), text: text };
+    this.setState({
+      tasks: [...this.state.tasks, newTask],
+      newTaskText: "",
+    });
+  };
+
+  render() {
+    return (
+      <div className={style.container}>
+        <h2 className={style.title}>Список завдань</h2>
+        <div className={style.box}>
+          <input
+            value={this.state.newTaskText}
+            onInput={this.handleInput}
+            className={style.input}
+            type="text"
+            placeholder="Введіть нове завдання"
+          />
+          <button
+            className={style.addBtn}
+            onClick={this.handleAdd}
+            type="button"
+          >
+            Додати
+          </button>
+        </div>
+        <ul className={style.list}>
+          {this.state.tasks.map(({ id, text }) => {
+            return (
+              <li className={style.item} key={id}>
+                {text}
+                <button
+                  className={style.delBtn}
+                  onClick={() => this.handleDelete(id)}
+                  type="button"
+                >
+                  Видалити
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
